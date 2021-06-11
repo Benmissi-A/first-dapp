68a672c9a4f9dd040498c3bfd57ee87d94e3b10e
Benmissi-A <aminebenmissi@hotmail.fr>
2021-06-11 10:37:30 +0200

    v2 from akermoon

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
 import { useContext, useState } from "react";
-import { Web3Context} from "web3-hooks";
-import {ethers} from 'ethers'
+import { Web3Context } from "web3-hooks";
+import { ethers } from "ethers";
+
 function App() {
   const [web3State, login] = useContext(Web3Context);
   const [ethBalance, setEthBalance] = useState(0);
   const [address, setAddress] = useState(ethers.constants.AddressZero);
-  const [eth2Send,setEth2Send] = useState(0)
-  //console.log(web3State)
+  const [eth2Send, setEth2Send] = useState(0);
+
   const handleClickGetBalance = async () => {
     try {
-      const balance = await web3State.provider.getBalance(address)
-      console.log(balance)
-      setEthBalance(balance);
-    }catch(e){
-      console.log(e)
+      const balance = await web3State.provider.getBalance(address);
+      setEthBalance(ethers.utils.formatEther(balance));
+    } catch (e) {
+      console.log(e);
     }
-  }
+  };
+
   const handleClickSend = async () => {
-    const weiAmount = ethBalance
-     try {
-       const tx =await web3State.signer.sendTransaction({
-         to: address,
-         value: weiAmount,
-       })
-       await tx.wait()
-       console.log('TX MINED')
-       console.log(tx);
-     } catch (e) {
-       console.log(e);
-     }
-  }
+    const weiAmount = ethers.utils.parseEther(eth2Send);
+    try {
+      const tx = await web3State.signer.sendTransaction({
+        to: address,
+        value: weiAmount,
+      });
+      await tx.wait();
+      console.log("TX MINED");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <>
-      <h1>Hello world</h1>
-      <p>Metamask installed {web3State.isMetaMask ? "yes" : "no"}</p>
-      <p>web3 injected {web3State.isWeb3 ? "yes" : "no"}</p>
+      <p>MetaMask installed: {web3State.isMetaMask ? "yes" : "no"}</p>
+      <p>Web3: {web3State.isWeb3 ? "injected" : "no-injected"}</p>
+      <p>logged: {web3State.isLogged ? "yes" : "no"}</p>
       {!web3State.isLogged && (
         <>
           <button onClick={login}>login</button>
@@ -42,29 +43,27 @@ function App() {
       )}
       <p>Network id: {web3State.chainId}</p>
       <p>Network name: {web3State.networkName}</p>
-      <p>Account: {web3State.account}</p>
+      <p>account: {web3State.account}</p>
       <p>Balance: {web3State.balance}</p>
-      <label htmlFor="balanceOf"></label>
+      <label htmlFor="balanceOf">Balance of:</label>
       <input
-        id="balenceOf"
+        id="balanceOf"
         type="text"
         value={address}
         placeholder="ethereum address"
-        onChange={(e) => {
-          setAddress(e.target.value);
-        }}
+        onChange={(event) => setAddress(event.target.value)}
       />
-      <button onClick={handleClickGetBalance}>get Balance</button>
+      <button onClick={handleClickGetBalance}>get balance</button>
       <p>
-        Balance Of {address}: {ethers.utils.formatEther(ethBalance)} ETHER
+        Balance of {address}: {ethBalance} ETHER
       </p>
-      <label htmlFor="eth2Send"></label>
+      <label htmlFor="eth2send">send to: {address}</label>
       <input
         id="eth2Send"
         type="text"
-        value={ethers.utils.formatEther(eth2Send)}
-        placeholder="ethereum amount"
-        onChange={(e) => setEth2Send(ethers.utils.parseEther(e.target.value))}
+        placeholder="ether ammount"
+        value={eth2Send}
+        onChange={(event) => setEth2Send(event.target.value)}
       />
       <button onClick={handleClickSend}>send</button>
     </>
