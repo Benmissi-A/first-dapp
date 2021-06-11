d1313076f019985e38490c6b2ece35afa8d2e79f
Benmissi-A <aminebenmissi@hotmail.fr>
2021-06-11 10:49:05 +0200

    contract deployed abi in dapp

---
+++ src/contracts/Counter.js
@@ -0,0 +1,29 @@
+[
+  {
+    inputs: [],
+    name: "decrement",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "increment",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "counter",
+    outputs: [
+      {
+        internalType: "int256",
+        name: "",
+        type: "int256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
