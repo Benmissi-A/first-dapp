d62ccb396a06b5a8e9e875630a30398421b16a26
Benmissi-A <aminebenmissi@hotmail.fr>
2021-06-11 12:11:52 +0200

    its ok

---
+++ src/Counter.js
@@ -0,0 +1,47 @@
+import { useContext, useState } from "react";
+import { CounterContext } from "./App";
+
+const Counter = () => {
+  const counter = useContext(CounterContext);
+  const [count, setCount] = useState(0);
+
+  const handleClickGet = async () => {
+    try {
+      console.log("trigger get count");
+      const currentCount = await counter.counter();
+      setCount(currentCount.toString());
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const handleClickIncrement = async () => {
+    try {
+      console.log("trigger + count");
+      const currentCount = await counter.increment();
+      setCount(currentCount.toString());
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const handleClickDecrement = async () => {
+    try {
+      console.log("trigger - count");
+      const currentCount = await counter.decrement();
+      setCount(currentCount.toString());
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <>
+      <h1>Counter</h1>
+      <p>count: {count}</p>
+      <button onClick={handleClickGet}>get count</button>
+      <button onClick={handleClickIncrement}>+</button>
+      <button onClick={handleClickDecrement}>-</button>
+    </>
+  );
+};
+
+export default Counter;
--- src/contracts/Counter.js
+++ src/contracts/Counter.js
@@ -1,4 +1,6 @@
-[
+//deployed on Rinkeby
+export const counterAddress = '0x8847fc4b8671781a4a4beb45da1c29664721e53a'
+export const counterAbi = [
   {
     inputs: [],
     name: "decrement",
